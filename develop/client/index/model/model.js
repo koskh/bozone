c398cdb4b2f1c3a273732efd1adfe1a76bda098d
koskh <koskh@yandex.ru>
2015-11-23 01:03:27 +0500

    init commit

---
+++ develop/client/index/model/model.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports =  Backbone.Model.extend({
+    defaults: {
+        name:'Test name',
+        address: 'Red patriots str, 2-22'
+    }
+});
---
+++ public/js/client/bootstrap.js
@@ -0,0 +1,6 @@
+'use strict';
+var DEBUG = true;
+var _  = require('underscore');
+var Backbone = require ('backbone');
+var $ = Backbone.$ = require ('jquery');
+var Marionette = require('backbone.marionette');
