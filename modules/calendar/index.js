e4e86a8075d1351e8e7c768949ca223fc4797ea2
koskh <koskh@yandex.ru>
2015-11-24 17:36:45 +0500

    # module/calendar/ заполнение днями на нужный месяц

--- modules/calendar/index.js
+++ modules/calendar/index.js
@@ -1,16 +1,15 @@
 'use strict';
 
-//var  Day = require ('day.model');
 
 var Month = require ('./collection/month.collection');
+
 var month = new Month();
 
 var IndexView = require('./view/index.view');
+
 var indexView = new IndexView({
     el: '#calendar',
-
     collection: month
-
 });
 
 indexView.render();
